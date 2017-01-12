22a359987cabbf5856ff89ea1ef89e125f14a66b
kering <kering@ridi.com>
2017-01-12 18:09:49 +0900

    add RxBus.unregister()

--- src/main/kotlin/com/ridi/books/rxbus/RxBus.kt
+++ src/main/kotlin/com/ridi/books/rxbus/RxBus.kt
@@ -1,22 +1,33 @@
 package com.ridi.books.rxbus
 
-import rx.Subscription
 import rx.functions.Action1
 import rx.subjects.PublishSubject
 import rx.subjects.SerializedSubject
+import rx.subscriptions.CompositeSubscription
 
 /**
  * Created by kering on 2017. 1. 12..
  */
 object RxBus {
-    private val busSubject = SerializedSubject<Any, Any>(PublishSubject.create())
+    private val eventsSubject = SerializedSubject<Any, Any>(PublishSubject.create())
+    private val subscriptions = hashMapOf<Any, CompositeSubscription>()
 
     @Suppress("UNCHECKED_CAST")
-    fun <T> register(eventClass: Class<T>, onNext: Action1<T>): Subscription {
-        return busSubject.filter { event -> event.javaClass == eventClass }
+    fun <T> register(owner: Any, eventClass: Class<T>, onNext: Action1<T>) {
+        val subscription = eventsSubject.filter { event -> event.javaClass == eventClass }
                 .map { obj -> obj as T }
                 .subscribe(onNext)
+        subscriptions[owner]?.run {
+            add(subscription)
+        } ?: run {
+            subscriptions[owner] = CompositeSubscription(subscription)
+        }
     }
 
-    fun post(event: Any) = busSubject.onNext(event)
+    fun unregister(owner: Any) {
+        subscriptions[owner]?.clear()
+        subscriptions.remove(owner)
+    }
+
+    fun post(event: Any) = eventsSubject.onNext(event)
 }
--- src/test/kotlin/com/ridi/books/rxbus/RxBusTest.kt
+++ src/test/kotlin/com/ridi/books/rxbus/RxBusTest.kt
@@ -13,21 +13,35 @@ class RxBusTest {
 
     @Test
     fun simpleEventTest() {
-        RxBus.register(Event::class.java, Action1 {
+        RxBus.register(this, Event::class.java, Action1 {
             Assert.assertTrue(true)
         })
         RxBus.post(Event())
+        RxBus.register(this, Event::class.java, Action1 {
+            Assert.assertTrue(false)
+        })
+        RxBus.unregister(this)
+        RxBus.post(Event())
+    }
 
-
+    @Test
+    fun multiSubscriptionTest() {
+        RxBus.register(this, Event::class.java, Action1 {})
+        RxBus.register(this, Event::class.java, Action1 {
+            Assert.assertTrue(true)
+        })
+        RxBus.post(Event())
+        RxBus.unregister(this)
     }
 
     class IntValueEvent(val value: Int)
 
     @Test
     fun intValueEventTest() {
-        RxBus.register(IntValueEvent::class.java, Action1 { e ->
+        RxBus.register(this, IntValueEvent::class.java, Action1 { e ->
             Assert.assertEquals(e.value, 100)
         })
         RxBus.post(IntValueEvent(100))
+        RxBus.unregister(this)
     }
 }
