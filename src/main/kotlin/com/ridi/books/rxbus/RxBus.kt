42203e9c7a1aa14a04fb22de3bef30aea85377cc
kering <kering@ridi.com>
2017-01-26 16:00:29 +0900

    support sticky events, add test for sticky events, schedulers

--- src/main/kotlin/com/ridi/books/rxbus/RxBus.kt
+++ src/main/kotlin/com/ridi/books/rxbus/RxBus.kt
@@ -1,26 +1,66 @@
 package com.ridi.books.rxbus
 
+import rx.Observable
 import rx.Scheduler
 import rx.Subscription
 import rx.functions.Action1
 import rx.schedulers.Schedulers
 import rx.subjects.PublishSubject
 import rx.subjects.SerializedSubject
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Created by kering on 2017. 1. 12..
  */
 object RxBus {
     private val subject = SerializedSubject<Any, Any>(PublishSubject.create())
-
-    var defaultScheduler = Schedulers.immediate()
+    private val stickyEventMap = ConcurrentHashMap<Class<*>, Any>()
 
     @JvmStatic
     @JvmOverloads
     fun <T> subscribe(eventClass: Class<T>, callback: Action1<T>,
-                      scheduler: Scheduler = defaultScheduler): Subscription =
+                      scheduler: Scheduler = Schedulers.immediate()): Subscription =
             subject.ofType(eventClass).observeOn(scheduler).subscribe(callback)
+
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    @JvmOverloads
+    fun <T> subscribeSticky(eventClass: Class<T>, callback: Action1<T>,
+                            scheduler: Scheduler = Schedulers.immediate()): Subscription {
+        synchronized(stickyEventMap) {
+            val observable = subject.ofType(eventClass)
+            return (stickyEventMap[eventClass]?.let { lastEvent ->
+                observable.mergeWith(Observable.create { subscriber ->
+                    subscriber.onNext(lastEvent as T)
+                })
+            } ?: observable).subscribeOn(scheduler).subscribe(callback)
+        }
+    }
 
     @JvmStatic
     fun post(event: Any) = subject.onNext(event)
+
+    @JvmStatic
+    fun postSticky(event: Any) {
+        synchronized(stickyEventMap) {
+            stickyEventMap.put(event.javaClass, event)
+        }
+        post(event)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    fun <T> getStickyEvent(eventClass: Class<T>): T? {
+        synchronized(stickyEventMap) {
+            return stickyEventMap[eventClass] as T?
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    @JvmStatic
+    fun <T> removeStickyEvent(eventClass: Class<T>): T? {
+        synchronized(stickyEventMap) {
+            return stickyEventMap.remove(eventClass) as T?
+        }
+    }
 }
--- src/test/kotlin/com/ridi/books/rxbus/RxBusTest.kt
+++ src/test/kotlin/com/ridi/books/rxbus/RxBusTest.kt
@@ -1,9 +1,11 @@
 package com.ridi.books.rxbus
 
+import net.jodah.concurrentunit.Waiter
 import org.junit.Assert
 import org.junit.Test
 
 import rx.functions.Action1
+import rx.schedulers.Schedulers
 import rx.subscriptions.CompositeSubscription
 
 /**
@@ -59,5 +61,60 @@ class RxBusTest {
         Assert.assertEquals(countChildEvent, 1)
 
         subscription.clear()
+    }
+
+    @Test
+    fun testSticky() {
+        var count = 0
+        val event = Event()
+        RxBus.postSticky(event)
+        Assert.assertEquals(event, RxBus.getStickyEvent(Event::class.java))
+
+        var subscription = RxBus.subscribeSticky(Event::class.java, Action1 { e ->
+            Assert.assertEquals(e, event)
+            count++
+        })
+        Assert.assertEquals(count, 1)
+        RxBus.post(event)
+        Assert.assertEquals(count, 2)
+        subscription.unsubscribe()
+        Assert.assertEquals(RxBus.removeStickyEvent(Event::class.java), event)
+
+        subscription = RxBus.subscribeSticky(Event::class.java, Action1 { count++ })
+        Assert.assertEquals(count, 2)
+        subscription.unsubscribe()
+
+        RxBus.postSticky(event)
+        subscription = RxBus.subscribe(Event::class.java, Action1 { e ->
+            Assert.assertEquals(e, event)
+            count++
+        })
+        Assert.assertEquals(count ,2)
+        RxBus.post(event)
+        Assert.assertEquals(count, 3)
+        RxBus.postSticky(event)
+        Assert.assertEquals(count, 4)
+        subscription.unsubscribe()
+        Assert.assertEquals(RxBus.removeStickyEvent(Event::class.java), event)
+    }
+
+    @Test
+    fun testScheduler() {
+        val waiter = Waiter()
+        val testThread = Thread.currentThread()
+        val callback = Action1<Event> {
+            Assert.assertNotEquals(testThread.id, Thread.currentThread().id)
+            waiter.resume()
+        }
+
+        var subscription = RxBus.subscribe(Event::class.java, callback, Schedulers.newThread())
+        RxBus.post(Event())
+        waiter.await()
+        subscription.unsubscribe()
+
+        RxBus.postSticky(Event())
+        subscription = RxBus.subscribeSticky(Event::class.java, callback, Schedulers.newThread())
+        waiter.await()
+        subscription.unsubscribe()
     }
 }
