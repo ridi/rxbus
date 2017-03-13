a63e58c1371db9da0d9dfdcf9df3c91c0a47b998
kering <kering@ridi.com>
2017-03-13 19:32:18 +0900

    Add another Activity.rxBusObservable method takes an ActivityEvent parameter(bindUntil)

--- rxbus-android/src/main/kotlin/com/ridi/books/rxbus/android/RxBusAndroid.kt
+++ rxbus-android/src/main/kotlin/com/ridi/books/rxbus/android/RxBusAndroid.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.Fragment
 import android.view.View
 import com.ridi.books.rxbus.RxBus
+import com.trello.rxlifecycle2.android.ActivityEvent
 import com.trello.rxlifecycle2.android.RxLifecycleAndroid
 import io.reactivex.Observable
 
@@ -16,6 +17,12 @@ fun <T> Activity.rxBusObservable(eventClass: Class<T>,
                                  sticky: Boolean = false, priority: Int = 0): Observable<T> =
         RxBus.asObservable(eventClass, sticky, priority)
                 .compose(RxActivityLifecycleProviderPool.provider(this).bindToLifecycle())
+
+@JvmOverloads
+fun <T> Activity.rxBusObservable(eventClass: Class<T>, sticky: Boolean = false,
+                                 priority: Int = 0, bindUntil: ActivityEvent): Observable<T> =
+        RxBus.asObservable(eventClass, sticky, priority)
+                .compose(RxActivityLifecycleProviderPool.provider(this).bindUntilEvent(bindUntil))
 
 @JvmOverloads
 fun <T> Fragment.rxBusObservable(eventClass: Class<T>,
